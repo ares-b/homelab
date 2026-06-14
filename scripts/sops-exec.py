92550901469dc34e96547cb0ff1f4a63ef8ba1f9
Ares <learslane97@gmail.com>
2026-06-14 22:23:30 +0200

    fix(scripts): unpack sys.argv correctly in sops-exec.py

--- scripts/sops-exec.py
+++ scripts/sops-exec.py
@@ -8,7 +8,7 @@ import yaml
 if len(sys.argv) < 4:
     sys.exit(f"usage: {sys.argv[0]} <yaml-file> <VAR_PREFIX> <cmd> [args...]")
 
-yaml_file, prefix, *cmd = sys.argv[1], sys.argv[2], sys.argv[3:]
+yaml_file, prefix, *cmd = sys.argv[1:]
 
 with open(yaml_file) as f:
     data = yaml.safe_load(f) or {}
